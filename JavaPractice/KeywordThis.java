be115aadb0d316345707255a3b11791c6843336c
farhaddubey <farhaddubey003@gmail.com>
2024-08-18 09:07:53 +0530

    com

---
+++ JavaPractice/KeywordThis.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+// class Student{
+//     int rollno;
+//     String name;
+//     float fee;
+//     Student(int rollno, String name, float fee){
+//         this.rollno=rollno;
+//         this.name=name;
+//         this.fee=fee;
+//     }
+//     //this is requied if & only if the instance variable and local variables are same. 
+
+//     Student(no, fullname, money){
+//         rollno=no;
+//         name=fullname;
+//         fee=money;
+//     }
+// }
+class A5{
+    void m(){
+        System.out.println(this);
+    }
+}
+public class KeywordThis{
+    public static void main(String[] args){
+        A5 obj=new A5();
+        System.out.println(obj); //prints the reference id of A5's obj
+        obj.m();
+        // Student s1=new Student(111, "Anukur", 5000f);
+
+    }
+}
